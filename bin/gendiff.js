7450500dbdcee6a1f704b9eff57909b4e184b897
Glazoff <glazov_evgen@mail.ru>
2025-08-21 21:25:13 +0300

    step_2/add_opt_arg_gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import { program } from 'commander'
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .usage('[options]')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
 
 program.parse()
